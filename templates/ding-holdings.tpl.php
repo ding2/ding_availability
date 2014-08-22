7acce886b4954084b32eb1e8489dd649a9bf7f71
Martin Cording <guddo@me.com>
2014-08-22 16:28:14 +0200

    DDBEASY-32 - Waiting time when no items available.
    Signed-off-by: Martin Cording <guddo@me.com>

--- templates/ding-holdings.tpl.php
+++ templates/ding-holdings.tpl.php
@@ -13,4 +13,5 @@ $total_text = format_plural($total_count, 'We have 1 copy.', 'We have @count cop
 $reserved_text = format_plural($reserved_count, 'There is 1 user in queue to loan the material.', 'There are @count users in queue to loan the material.');
 ?>
 <p><?php print "$total_text $reserved_text"; ?></p>
+<p><?php print $closest_loan; ?></p>
 <?php print render($holdings); ?>
