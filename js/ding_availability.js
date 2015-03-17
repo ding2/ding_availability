dc1fb96e3a0ee2ee0a36fdd41d146f678313f57b
Martin Cording <guddo@me.com>
2015-03-17 19:13:18 +0100

    DDBEASY-116 - Refactored the availability and reservable logic.

--- js/ding_availability.js
+++ js/ding_availability.js
@@ -83,17 +83,17 @@
     var available = false;
     var reservable = false;
     var is_internet = false;
+    var element = $('#' + id);
+    element.removeClass('pending').addClass('processed');
+
     $.each(entity_ids, function(index, entity_id) {
+      // Reserve button
+      var reserver_btn = element.parents('.ting-object:first').find('a[id$=' + entity_id + '].reserve-button');
+
       if (Drupal.DADB[entity_id]) {
         available = available || Drupal.DADB[entity_id]['available'];
         reservable = reservable || Drupal.DADB[entity_id]['reservable'];
         is_internet = is_internet || Drupal.DADB[entity_id]['is_internet'];
-
-        var element = $('#' + id);
-        element.removeClass('pending').addClass('processed');
-
-        // Reserve button
-        var reserver_btn = element.parents('.ting-object:first').find('.reserve-button');
 
         if (available) {
           element.addClass('available');
@@ -121,7 +121,7 @@
           }
         }
 
-        if (!available && !reservable) {
+        else {
           element.addClass('not-reservable');
 
           // Add class to reserve button
@@ -129,6 +129,9 @@
             reserver_btn.addClass('not-reservable');
           }
         }
+      }
+      else {
+        reserver_btn.addClass('not-reservable');
       }
     });
   }
